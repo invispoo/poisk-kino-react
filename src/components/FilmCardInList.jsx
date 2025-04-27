6d5ba2e84b56f726ee0737812ed0c37d1ba5ab60
okashapova <okashapova@parseq.pro>
2025-04-27 14:53:04 +0300

    positioned the film cards and their covers

--- src/components/FilmCardInList.jsx
+++ src/components/FilmCardInList.jsx
@@ -1,3 +1,20 @@
-export default function FilmCardInList ({film}) {
- return <div>{film.name}</div>
+import "./FilmCardInList.css";
+import Icon from "@mdi/react";
+import { mdiStarShootingOutline } from "@mdi/js";
+
+export default function FilmCardInList({ film }) {
+  return (
+    <div className="card">
+      <div className="title">{film.name}</div>
+      <img
+        src={film.poster.previewUrl}
+        alt={film.name}
+        className="picture"
+      />
+      <div className="rating">
+        <Icon path={mdiStarShootingOutline} size={1} />
+        <div>{film.rating.kp}</div>
+      </div>
+    </div>
+  );
 }
